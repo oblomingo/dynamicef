57b8fc61e31c57cee1e75350e17f51817f49619f
oblomingo <oblomingo@gmail.com>
2019-05-28 18:47:51 +0300

    Add project files.

---
+++ DynamicEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
